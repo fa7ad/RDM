a1fed13348460a8b84122736beb3e8294229e1bd
aryamccarthy <arya.mccarthy@gmail.com>
2016-10-02 14:08:35 -0500

    Added support for Dark Mode

--- SRApplicationDelegate.m
+++ SRApplicationDelegate.m
@@ -237,15 +237,14 @@ void DisplayReconfigurationCallback(CGDirectDisplayID cg_id,
 {
 //	NSLog(@"Finished launching");
 	statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength: NSSquareStatusItemLength] retain];
-
+	
 	NSImage* statusImage = [NSImage imageNamed: @"StatusIcon"];
-	statusItem.button.image = statusImage;
-	[statusItem.button.cell setHighlightsBy: NSContentsCellMask];
+	[statusItem setImage: statusImage];
+	[statusItem setHighlightMode: YES];
 
-  BOOL supportsDarkMenu = !(floor(NSAppKitVersionNumber) < 1343);
+  BOOL supportsDarkMenu = !(floor(NSAppKitVersionNumber) < 1343);  // NSAppKitVersionNumber10_10
   if (supportsDarkMenu) {
-    // [[statusItem image] setTemplate:YES];
-	  [statusItem.button.image setTemplate: YES];
+    [[statusItem image] setTemplate:YES];
   }
 
 	[self refreshStatusMenu];
